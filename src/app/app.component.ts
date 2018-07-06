d0d310d25a95e56eeee00807562b61ac7b14df09
Krzysztof Kozlowski <kozlowski.k@o2.pl>
2018-07-06 11:04:05 +0200

    stronka

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentViewName = 'home';
+  selectView(name: string){
+    this.currentViewName = name;
+  }
 }
